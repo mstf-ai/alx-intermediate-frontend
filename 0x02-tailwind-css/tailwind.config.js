f173523bbf7a9d7615b5b64ee0b5e85113e7d796
mstf-ai <albrnsmstfy252@gmail.com>
2025-11-01 18:01:53 +0200

    M

---
+++ 0x02-tailwind-css/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	  content: ["./src/**/*.{html,js}"],  // مسار ملفات HTML/JS لاكتشاف الكلاسات
+	  theme: {
+		      extend: {},
+		    },
+	  plugins: [],
+}
+
